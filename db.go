ddcf527a710688b1569232cf7feafeac85c5d754
Gleb Dubrovin <termiusone@gmail.com>
2022-05-07 17:48:11 +0300

    add ping method and other setters #21

--- db.go
+++ db.go
@@ -76,6 +76,47 @@ func (db *DB) Close() error {
 	return db.DB.Close()
 }
 
+// PingContext verifies a connection to the database is still alive,
+// establishing a connection if necessary.
+func (db *DB) PingContext(ctx context.Context) error {
+	return db.DB.PingContext(ctx)
+}
+
+// SetConnMaxIdleTime sets the maximum amount of time a connection may be idle.
+//
+// Expired connections may be closed lazily before reuse.
+//
+// If d <= 0, connections are not closed due to a connection's idle time.
+func (db *DB) SetConnMaxIdleTime(d time.Duration) {
+	db.DB.SetConnMaxIdleTime(d)
+}
+
+// SetMaxIdleConns sets the maximum number of connections in the idle
+// connection pool.
+//
+// If MaxOpenConns is greater than 0 but less than the new MaxIdleConns,
+// then the new MaxIdleConns will be reduced to match the MaxOpenConns limit.
+//
+// If n <= 0, no idle connections are retained.
+//
+// The default max idle connections is currently 2. This may change in
+// a future release.
+func (db *DB) SetMaxIdleConns(n int) {
+	db.DB.SetMaxIdleConns(n)
+}
+
+// SetMaxOpenConns sets the maximum number of open connections to the database.
+//
+// If MaxIdleConns is greater than 0 and the new MaxOpenConns is less than
+// MaxIdleConns, then MaxIdleConns will be reduced to match the new
+// MaxOpenConns limit.
+//
+// If n <= 0, then there is no limit on the number of open connections.
+// The default is 0 (unlimited).
+func (db *DB) SetMaxOpenConns(n int) {
+	db.DB.SetMaxOpenConns(n)
+}
+
 func wrapDriver(driverName string, hook dbhook.Hook) (string, error) {
 	if hook == nil { // skip wrapping for empty hook.
 		return driverName, nil
